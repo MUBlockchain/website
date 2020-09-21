9a1ba1c1b3b674cfefe9ee3fe7f1a161eb36bc62
Ubuntu <ubuntu@ip-172-31-45-200.us-east-2.compute.internal>
2020-09-21 15:10:11 +0000

    done

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
