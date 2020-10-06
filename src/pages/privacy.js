c92e4d833857e972e3f83a89f0e3032ac3a19e44
Jack Gilcrest <gilcest.jack@gmail.com>
2020-10-06 18:13:25 -0400

    privacy page

---
+++ src/pages/privacy.js
@@ -0,0 +1,21 @@
+import React from "react"
+import {Carousel} from "react-bootstrap"
+import SEO from "../components/seo"
+import MUBCHeader from "../components/MUBCHeader"
+import MUBCNavbar from "../components/MUBCNavbar"
+import MUBCFooter from "../components/MUBCFooter"
+
+const Privacy = () => {
+    return (
+        <div>
+            <SEO title="Privacy Policy" description="Miami University Blockchain Club Privacy Policy"/>
+            <MUBCHeader/>
+            <MUBCNavbar/>
+	    <p>
+	    	This page will hold our privacy policy soon. We are compelled to create this page for our Android & iOS App!
+	    </p>
+            <MUBCFooter/>
+        </div>
+    )
+}
+export default Privacy
